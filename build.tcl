9f14daef052b8ff881803928d48644c25c824b48
Danilo Chang <ray2501@gmail.com>
2020-03-12 22:05:24 +0800

    Update to 2.7.1

--- build.tcl
+++ build.tcl
@@ -1,8 +1,8 @@
 #!/usr/bin/tclsh
 
 set arch "x86_64"
-set base "Pgtcl-2.6.3"
-set fileurl "https://github.com/flightaware/Pgtcl/archive/v2.6.3.tar.gz"
+set base "Pgtcl-2.7.1"
+set fileurl "https://github.com/flightaware/Pgtcl/archive/v2.7.1.tar.gz"
 
 set var [list wget $fileurl -O $base.tar.gz]
 exec >@stdout 2>@stderr {*}$var
